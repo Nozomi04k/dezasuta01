1e1b0ad5b74ae3b5d2324a2556a58a8345efc865
Nozomi04k <140169140+Nozomi04k@users.noreply.github.com>
2023-10-21 01:32:31 +0900

    ポートフォリオ

---
+++ kuroki_ポートフォリオ/js/script.js
@@ -0,0 +1,28 @@
+(function (d) {
+    var config = {
+        kitId: 'ztc4geb',
+        scriptTimeout: 3000,
+        async: true
+    },
+        h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
+})(document);
+
+$(function () {
+    // マウスホイールを横スクロールにする
+    const scrollElement = document.querySelector(".wrapper");
+
+    scrollElement.addEventListener("wheel", (e) => {
+        if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
+
+        const maxScrollLeft = scrollElement.scrollWidth - scrollElement.clientWidth;
+
+        if (
+            (scrollElement.scrollLeft <= 0 && e.deltaY < 0) ||
+            (scrollElement.scrollLeft >= maxScrollLeft && e.deltaY > 0)
+        )
+            return;
+
+        e.preventDefault();
+        scrollElement.scrollLeft += e.deltaY;
+    });
+});
